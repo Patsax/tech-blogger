2deb674ac75c045b592326b0810a7e8e9c45e324
Patrick <patasax22@yahoo.com>
2021-11-11 16:56:01 -0500

    controllers code cleanup

--- controllers/index.js
+++ controllers/index.js
@@ -4,12 +4,12 @@ const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
 const dashboardRoutes = require("./dashboard-routes");
 
-// router.use("/home", homeRoutes);
+router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
-// router.use("/dashboard", dashboardRoutes);
+router.use("/dashboard", dashboardRoutes);
 
-router.use((req, res) => {
-    res.send("<h1>Wrong Route!</h1>");
-});
+// router.use((req, res) => {
+//     res.send("<h1>Wrong Route!</h1>");
+// });
 
 module.exports = router;
